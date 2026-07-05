2c0dab0e828567f015fd28af2c5bb1485d687b15
Bijoy Mandal <bijoymandal00@gmail.com>
2026-07-06 02:29:56 +0530

    feat: update currency formatting to Indian Rupee in Deals component

--- client/src/pages/Deals.jsx
+++ client/src/pages/Deals.jsx
@@ -150,9 +150,9 @@ const Deals = () => {
   };
 
   const formatCurrency = (val) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'INR',
       maximumFractionDigits: 0,
     }).format(val);
   };
