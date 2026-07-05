4073d89c9a21da3664644a622b9878cc039dcd42
Bijoy Mandal <bijoymandal00@gmail.com>
2026-07-06 03:43:22 +0530

    feat: update currency formatting to Indian Rupee in Dashboard component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -61,9 +61,9 @@ const Dashboard = () => {
 
   // Formatting helper
   const formatCurrency = (val) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'INR',
       maximumFractionDigits: 0
     }).format(val);
   };
